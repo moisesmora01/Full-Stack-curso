82aee5d7dfc291d3bc613c5a2130158914d35e7b
moisesmora01 <idiomasmoises@gmail.com>
2025-07-05 10:27:03 -0500

    Updated j.s

---
+++ Modulo 0/Clase 5 JavaScript/Variables.js
@@ -0,0 +1,39 @@
+//tipos de datos: 
+
+//String:
+
+var bootcamp = 'henry';
+
+//Numeros: 
+
+var numeroentero = 17;
+
+var numeronegativo = -4;
+
+var numerodecimal= 11.24;
+
+//Booleanos: verdadero y falso
+
+var estoyaburrido = false;
+
+var estoyentendiendo = true;
+
+//Undefined
+
+var cajon1;
+
+
+//Null
+
+var sinnada = null;
+
+
+var nombre = "Camilo";
+var saludo = "Hola " + nombre;
+console.log(saludo);
+
+
+
+
+
+
---
+++ Modulo 0/Clase 5 JavaScript/operadoresdecomparacion.js
@@ -0,0 +1,11 @@
+console.log( 4 == 7);  
+console.log (4 < 1 );
+console.log (4 > 4); 
+console.log (4 == 7);
+
+console.log (3 == 3); 
+console.log (3 === 3); 
+console.log (3 == "3"); 
+console.log (3 === "3");
+console.log (7 == "7");
+console.log (7 === "7");
